33883b59eb7823232c3a39f1b394cd78c2ccbaf4
injectxr <rootrooty7@gmail.com>
2024-09-05 17:25:49 +0900

    0905 Private設定まで終了

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile,Depends
+from fastapi import FastAPI, File, UploadFile,Depends, Form, Query
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
@@ -15,7 +15,8 @@ from datetime import timedelta, datetime, timezone
 from app.data import JwtTokenData
 from app.tokens import *
 from datetime import datetime
- 
+from typing import Union
+
 
 print("init")
 #build 
@@ -65,41 +66,81 @@ async def main(request: Request):
 @app.post("/upload/")
 async def upload_image(file: UploadFile = File(...), token : JwtTokenData | None = Depends(get_token_or_none)):
     print(token)
-    file_content = await file.read()
-    dt_now = datetime.now()
-    
-    image_data = {
-        "uuid": token.uuid,
-        "original_filename": file.filename,
-        "file_data": Binary(file_content),
-        "upload_date" : dt_now,
-        "state" : "temp"
-    }
     try:
-        collection.insert_one(image_data)
+        if token.token_type == "Upload":
+            file_content = await file.read()
+            dt_now = datetime.now()
+            
+            image_data = {
+                "uuid": token.uuid,
+                "original_filename": file.filename,
+                "file_data": Binary(file_content),
+                "upload_date" : dt_now,
+                "state" : "Temp"
+            }
+            try:
+                collection.insert_one(image_data)
+            except errors.PyMongoError as e:
+                return {"status": [str(e)]}
+
+            return {"status":"OK"}
+        else:
+            return {"status":"error TOKEN type"}
+    except:
+        return {"status":"error TOKEN none"}
+    
+@app.put("/image_preference/{image_uuid}")
+async def post_image(image_uuid: str, state: str = Form(...)):  # Formを使ってstateを受け取る
+    try:
+        image_data = collection.find_one({"uuid": image_uuid})
+        
+        if image_data:
+            collection.update_one(
+                {"uuid": image_uuid},
+                {"$set": {"state": state}}
+            )
+            return {"status": "State updated", "new_state": state}
+        else:
+            return {"status": "Image not found"}
+
     except errors.PyMongoError as e:
         return {"status": [str(e)]}
-
-    return {"status":"OK"}
-
-
+    
 @app.get("/image/{image_uuid}")
-async def get_image(image_uuid: str):
+async def get_image(image_uuid: str,q: Union[str, None] = Query(default=None, max_length=50)):
     image_data = collection.find_one({"uuid": image_uuid})
     
     if image_data is None:
         raise HTTPException(status_code=404, detail="Image not found")
+    
+    image_state = image_data['state']
+    
+    if image_state == 'Public':
+        return _stream_image(image_data)
+        
+    elif image_state == 'Private':
+        if q is None:
+            raise HTTPException(status_code=400, detail="Token required")
+        
+        token = data.JwtTokenData(**jwt.decode(q, SECRET_KEY, algorithms=[ALGORITHM]))
+        if token.token_type == 'Access':
+            return _stream_image(image_data)
+        else:
+            raise HTTPException(status_code=403, detail="Invalid token type")
+    
+    return {"status": "Image not Public", "state": image_state}
 
+
+def _stream_image(image_data):
     file_data = image_data['file_data']
     file_extension = image_data['original_filename'].split('.')[-1]
     file_stream = io.BytesIO(file_data)
     media_type = f"image/{file_extension}"
-
     return StreamingResponse(file_stream, media_type=media_type)
 
 @app.get("/test/token")
 async def test_token():
-    return create_token(token_type="unnko",expires_delta=timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES))
+    return create_token(token_type="Upload",expires_delta=timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES))
 
 @app.get("/test/get")
 async def test_get(token : JwtTokenData | None = Depends(get_token)):
